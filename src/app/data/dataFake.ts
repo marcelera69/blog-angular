f68737ee827e0506623f2997b0fcc4fd8f18061a
Marcelo Soares <ceeloalves@hotmail.com>
2024-07-01 10:46:46 -0300

    finalizando a criação e rotas da pagina de conteúdo

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+    {
+        "id":1,
+        "photo":"aaa",
+        "title": "SAIU NOVO FILME",
+        "description": "ola"
+    },
+    {
+        "id":2,
+        "photo":"bbbbbbbb",
+        "title": "SAIU NOVA SERIE",
+        "description": "ola"
+    },
+    {
+        "id":3,
+        "photo":"CCCCCC ",
+        "title": "NAO SAIU NOVA NADA",
+        "description": "ola"
+    },
+]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
+  photoCover:string = "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/0A2C59BE2EF8E3C95BD6A69AE050198F4E44123D4B5C070DA3F62EE076F4AD92/scale?width=1200&amp;aspectRatio=1.78&amp;format=webp"
+  contentTitle:string = "MINHA NOTICIA"
+  contentDescription:string = "ola mundo"
+
   constructor() { }
 
   ngOnInit(): void {
